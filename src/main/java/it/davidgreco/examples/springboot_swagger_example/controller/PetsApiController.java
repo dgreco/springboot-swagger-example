e13074e4cec4e1f3c3122353515f841c43b13b94
dgreco <greco@acm.org>
2018-09-03 19:36:53 +0200

    added the sonatype snapshot repository

--- src/main/java/it/davidgreco/examples/springboot_swagger_example/controller/PetsApiController.java
+++ src/main/java/it/davidgreco/examples/springboot_swagger_example/controller/PetsApiController.java
@@ -9,37 +9,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class PetsApiController implements PetsApi {
 
+    private final PetsApiService service;
+
     @Autowired
-    protected PetsApiService petsApiService;
-
-    public ResponseEntity<Pet> addPet(@ApiParam(value = "Pet to add to the store" ,required=true )  @Valid @RequestBody NewPet pet) {
-        Pet newPet = petsApiService.addPet(pet);
-        return new ResponseEntity<Pet>(newPet, HttpStatus.OK);
+    public PetsApiController(PetsApiService petsApiService) {
+        this.service = petsApiService;
     }
 
-    public ResponseEntity<Pet> findPetById(@ApiParam(value = "ID of pet to fetch",required=true ) @PathVariable("id") Long id) {
-        Pet pet = petsApiService.findPetById(id);
-
-        return new ResponseEntity<Pet>(pet,HttpStatus.OK);
+    public CompletableFuture<ResponseEntity<Pet>> addPet(@ApiParam(value = "Pet to add to the store" ,required=true )  @Valid @RequestBody NewPet pet) {
+        Pet newPet = service.addPet(pet);
+        return CompletableFuture.completedFuture(new ResponseEntity<>(newPet, HttpStatus.OK));
     }
 
-    public ResponseEntity<List<Pet>> findPets(@ApiParam(value = "tags to filter by") @RequestParam(value = "tags", required = false) List<String> tags,
-        @ApiParam(value = "maximum number of results to return") @RequestParam(value = "limit", required = false) Integer limit) {
-        // dummy code
-        List<Pet> result = new ArrayList<>();
-        result.addAll(petsApiService.findPets(tags, limit));
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    @RequestMapping(value = "/pets/{id}",
+            produces = { "application/json" },
+            method = RequestMethod.GET)
+    public CompletableFuture<ResponseEntity<Pet>> findPetById(@ApiParam(value = "ID of pet to fetch",required=true) @PathVariable("id") Long id) {
+        Pet pet = service.findPetById(id);
+        return CompletableFuture.completedFuture(new ResponseEntity<>(pet,HttpStatus.OK));
+    }
+
+    public CompletableFuture<ResponseEntity<List<Pet>>> findPets(@ApiParam(value = "tags to filter by") @RequestParam(value = "tags", required = false) List<String> tags,
+                                                                 @ApiParam(value = "maximum number of results to return") @RequestParam(value = "limit", required = false) Integer limit) {
+        List<Pet> result = new ArrayList<>(service.findPets(tags, limit));
+        return CompletableFuture.completedFuture(new ResponseEntity<>(result, HttpStatus.OK));
     }
 
 }
